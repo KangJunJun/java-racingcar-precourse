45897b0a1c32283b119d0acf6804fb33edf47a5c
jhkang <serdozz@naver.com>
2022-10-07 16:00:54 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/racingcar/controller/Racing.java
+++ src/main/java/racingcar/controller/Racing.java
@@ -3,7 +3,6 @@ package racingcar.controller;
 import racingcar.model.Cars;
 import racingcar.model.Race;
 import racingcar.model.TryCount;
-
 import static racingcar.view.Input.*;
 import static racingcar.view.Print.*;
 
@@ -11,6 +10,15 @@ public class Racing {
 
     public void start(){
         Race race = new Race(inputName(), inputCount());
+        printRaceStart();
+        progressRacing(race);
+    }
+
+    private void progressRacing(Race race){
+        for (int count = 0; count < race.getCount(); count++) {
+            race.run();
+            printRun(race);
+        }
     }
 
     private Cars inputName(){
--- src/main/java/racingcar/view/Print.java
+++ src/main/java/racingcar/view/Print.java
@@ -1,9 +1,18 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+import racingcar.model.Race;
+
+import java.util.Collections;
+
 public class Print {
-    public static final String errorMark = "[ERROR] ";
-    public static final String InputRequestTryCount = "시도할 회수는 몇인가요?";
-    public static final String InputRequestName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String errorMark = "[ERROR] ";
+    private static final String separator = " : ";
+    private static final String distSeparator = "-";
+    private static final String InputRequestTryCount = "시도할 회수는 몇인가요?";
+    private static final String InputRequestName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String StartMsg = "실행 결과";
+
 
     public static void printError(String errorMsg) {
         System.out.println(errorMark + errorMsg);
@@ -14,5 +23,24 @@ public class Print {
     }
     public static void printInputRequestName() {
         System.out.println(InputRequestName);
+    }
+    public static void printRun(Race race){
+        for (Car car: race.getCars()) {
+            String result = car.getName() + separator + drawDistance(car.getDistance());
+            System.out.println(result);
+        }
+        System.out.println();
+    }
+
+    public static void printRaceStart(){
+        System.out.println(StartMsg);
+    }
+    
+    public static void printResult(){
+        //Todo : 최종 우승자 출력
+    }
+
+    private static String drawDistance(int distance){
+        return String.join("", Collections.nCopies(distance, distSeparator));
     }
 }
