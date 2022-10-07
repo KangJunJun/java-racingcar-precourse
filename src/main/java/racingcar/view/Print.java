6c86d19014956b4b4592eec8ca2451725fa686fa
jhkang <serdozz@naver.com>
2022-10-07 13:48:32 +0900

    fix: 에러마크 대소문자 변경

--- src/main/java/racingcar/view/Print.java
+++ src/main/java/racingcar/view/Print.java
@@ -1,7 +1,7 @@
 package racingcar.view;
 
 public class Print {
-    public static final String errorMark = "[Error] ";
+    public static final String errorMark = "[ERROR] ";
     public static final String InputRequestTryCount = "시도할 회수는 몇인가요?";
     public static final String InputRequestName = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
